98fc86988bb47b5ab12f412d0b36cc657e876072
Tuğrul Yüce <tugrulyuce80@gmail.com>
2024-03-11 18:02:07 +0300

    sorting2

--- src/home/Sorting.jsx
+++ src/home/Sorting.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Sorting = () => {
+const Sorting = ({setSort}) => {
   return (
     <div className='bg-gray-100 my-5 p-5 flex items-center justify-end'>
-     <select className='bg-white py-3 px-5'>
+     <select onChange={(e)=>setSort(e.target.value)} className='bg-white py-3 px-5'>
         <option disabled value="">Seçiniz</option>
         <option value="inc">Artan</option>
         <option value="dec">Azalan</option>
